df269d5b8d4fddba0605ebf9a29ddd7c4ce8805e
CheersMin <74691001+CheersMin@users.noreply.github.com>
2021-02-05 13:42:24 +0900

    Add files via upload

---
+++ 4.라이카/라이카 html/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	new fullpage('#fullPage',{
+		licensKey:"",
+		scrollingSpeed:1000,
+		navigation:true,
+		navigationTooltips:['intro','','라이카역사','','카메라소개','','스토어']
+		autoScrolling:true,
+		scrollHorizontally:true,
+	});
+})
